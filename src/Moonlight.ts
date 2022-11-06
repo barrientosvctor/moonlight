d7b9ca6b9075577458895747fcac87a3c80f6de6
uShawnTS <vibarrientosp@gmail.com>
2022-11-06 13:26:19 -0500

    Error method added

--- src/Moonlight.ts
+++ src/Moonlight.ts
@@ -1,6 +1,18 @@
-import { Client, Collection, GuildMember, User, WebhookClient } from "discord.js";
+import { BaseGuildTextChannel, Client, Collection, DiscordAPIError, EmbedBuilder, GuildMember, User, WebhookClient } from "discord.js";
 import { CommandHandler, EventHandler } from "./handlers";
 import { CommandBuilder } from "./structures/CommandBuilder";
+
+enum Type {
+    Command = 1,
+    Event = 2
+}
+
+interface ErrorDataOptions {
+    name: string;
+    type: Type;
+    channel?: BaseGuildTextChannel | WebhookClient;
+    error: Error | DiscordAPIError;
+}
 
 export class Moonlight extends Client {
     public constructor() {
@@ -18,8 +30,24 @@ export class Moonlight extends Client {
         this.login(process.env.BOT_TOKEN);
     }
 
+    public error(message: string, data: ErrorDataOptions) {
+        const errorEmbed = new EmbedBuilder();
+        errorEmbed.setDescription(`\`\`\`\n${data.error}\n\`\`\``)
+        errorEmbed.setFooter({ text: "Moonlight Logs", iconURL: this.user?.displayAvatarURL() })
+
+        if (data.type === Type.Command) {
+            errorEmbed.setTitle(`Comando: ${data.name}`);
+        } else {
+            errorEmbed.setTitle(`Evento: ${data.name}`);
+        }
+
+        this.hook.send({ embeds: [errorEmbed] });
+        if (data.channel) data.channel.send(`❌ ${message}`);
+        console.error(data.error);
+    }
+
     public isOwnerCommand(commandName: string): boolean {
-        const commandsList = this.categories.filter(ae => ae.name === "Desarrollador").map(ae => ae.commands.toString().slice(0, -3));
+        const commandsList: Array<string> = this.categories.filter(ae => ae.name === "Desarrollador").map(ae => ae.commands.toString().slice(0, -3));
 
         if (commandsList.includes(commandName)) return true;
         else return false;
@@ -30,3 +58,5 @@ export class Moonlight extends Client {
         else return false;
     }
 }
+
+export default Type
