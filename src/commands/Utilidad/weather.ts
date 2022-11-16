19d8c984ad1bdd381512c0868c88e11f194086f5
uShawnTS <vibarrientosp@gmail.com>
2022-11-16 11:53:27 -0500

    Building Utilities commands

--- src/commands/Utilidad/weather.ts
+++ src/commands/Utilidad/weather.ts
@@ -1,1 +1,18 @@
-console.log("WIP")
+import Type from "../../Moonlight";
+import { CommandBuilder } from "../../structures/CommandBuilder";
+
+export default new CommandBuilder({
+    name: "weather",
+    description: "Checa el clima con este comando.",
+    cooldown: 3,
+    aliases: ["wth"],
+    usage: "<ciudad | país>",
+    example: "New York",
+    async run(bot, msg, args) {
+        try {
+            return msg.reply("a")
+        } catch (err) {
+            bot.error("Hubo un error al intentar ejecutar el comando.", { name: this.name, type: Type.Command, channel: msg.channel, error: err });
+        }
+    }
+});
