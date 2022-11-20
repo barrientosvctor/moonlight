fb108b91f546cc9431336c3556c95d5217dd9af6
uShawnTS <vibarrientosp@gmail.com>
2022-11-20 18:34:16 -0500

    Nuke and purge command finished, new Moonlight version 3.2.1

---
+++ src/commands/Moderación/nuke.ts
@@ -0,0 +1,59 @@
+import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle, ComponentType, Interaction, TextChannel } from "discord.js";
+import Type from "../../Moonlight";
+import { CommandBuilder } from "../../structures/CommandBuilder";
+
+export default new CommandBuilder({
+    name: "nuke",
+    description: "Una limpíeza total del canal que menciones.",
+    cooldown: 3,
+    usage: "[#canal | ID]",
+    example: "#General",
+    enabled: true,
+    memberPerms: ["ManageGuild"],
+    botPerms: ["ManageChannels"],
+    async run(bot, msg, args, prefix, getUser, getMember, getChannel) {
+        try {
+            const channel = getChannel(args[1]) || msg.channel;
+            if (channel instanceof TextChannel) {
+                const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    new ButtonBuilder()
+                    .setLabel("Sí")
+                    .setStyle(ButtonStyle.Success)
+                    .setCustomId("si"),
+                    new ButtonBuilder()
+                    .setLabel("No")
+                    .setStyle(ButtonStyle.Danger)
+                    .setCustomId("no")
+                )
+
+                const confirmMsg = await msg.reply({ content: `¿Estás seguro de hacer esto?`, components: [row] });
+                const collector = confirmMsg.createMessageComponentCollector<ComponentType.Button>({ filter: (m: Interaction) => m.user.id === msg.author.id, max: 1, maxUsers: 1, time: 20_000 });
+
+                collector.once("collect", async res => {
+                    if (res.customId === "si") {
+                        await channel.clone().then(async ch => {
+                            ch.setParent(channel.parent);
+                            ch.setPosition(channel.position);
+                            await channel.delete().then(() => {
+                                return ch.send({ content: `**El canal ha explotado en mil pedazos...**`, files: [new AttachmentBuilder("https://imgur.com/LIyGeCR.gif", { name: "nuke.gif" })] })
+                            }).catch(() => {});
+                        }).catch(err => {
+                            console.error(err);
+                            msg.channel.send(bot.replyMessage(`Ocurrió un error al intentar eliminar el canal.`, { emoji: "warning" }));
+                        });
+                    } else {
+                        confirmMsg.edit({ content: bot.replyMessage("Operación cancelada.", { emoji: "error" }), components: [] }).then(message => setTimeout(() => message.delete(), 5000));
+                    }
+                });
+
+                collector.once("end", (collected, reason) => {
+                    // console.log({ collected, reason });
+                    if (reason === "limit") return;
+                    if (reason === "time") confirmMsg.edit({ content: bot.replyMessage(`Operación cancelada por inactividad.`, { emoji: "error" }), components: [] }).then(message => setTimeout(() => message.delete(), 5000));
+                });
+            } else return msg.reply(bot.replyMessage("Solo puedo hacer esta acción con canales de texto.", { emoji: "error" }));
+        } catch (err) {
+            bot.error("Ocurrió un error al intentar ejecutar el comando.", { name: this.name, type: Type.Command, channel: msg.channel, error: err });
+        }
+    }
+});
---
+++ src/commands/Moderación/purge.ts
@@ -0,0 +1,33 @@
+import { TextChannel } from "discord.js";
+import Type from "../../Moonlight";
+import { CommandBuilder } from "../../structures/CommandBuilder";
+
+export default new CommandBuilder({
+    name: "purge",
+    description: "Borra una cantidad de mensajes en el chat",
+    cooldown: 3,
+    usage: "<número>",
+    example: "20",
+    enabled: true,
+    memberPerms: ["ManageMessages"],
+    botPerms: ["ManageMessages"],
+    async run(bot, msg, args, prefix, getUser, getMember, getChannel) {
+        try {
+            if (msg.channel instanceof TextChannel) {
+                if (!args[1]) return msg.channel.send(bot.replyMessage("escribe el número de mensajes que vas a eliminar en este chat.", { mention: msg.author.username, emoji: "noargs" }));
+                if (!Number(args[1])) return msg.reply(bot.replyMessage("Debes de escribir un número válido.", { emoji: "error" }));
+                if (Number(args[1]) < 1 || Number(args[1]) > 100) return msg.reply(bot.replyMessage("La cantidad de mensajes a eliminar debe ser entre **1** y **100** mensajes.", { emoji: "error" }));
+                if (msg.deletable) await msg.delete();
+
+                await msg.channel.bulkDelete(parseInt(args[1]), true).then(() => {
+                    msg.channel.send(bot.replyMessage(`${parseInt(args[1])} mensajes eliminados.`, { emoji: "check" })).then(message => setTimeout(() => message.delete(), 10000)).catch(() => {});
+                }).catch(err => {
+                    console.error(err);
+                    msg.channel.send(bot.replyMessage("Ocurrió un error al intentar eliminar los mensajes.", { emoji: "warning" }));
+                });
+            } else return;
+        } catch (err) {
+            bot.error("Ocurrió un error al intentar ejecutar el comando.", { name: this.name, type: Type.Command, channel: msg.channel, error: err });
+        }
+    }
+});
