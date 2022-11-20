e78f71cf33ffcc7132ea8ddf900b9c9b560f9008
uShawnTS <vibarrientosp@gmail.com>
2022-11-20 14:00:37 -0500

    Mdn command finished

--- src/commands/Desarrollador/mdn.ts
+++ src/commands/Desarrollador/mdn.ts
@@ -1,36 +1,32 @@
-import { EmbedBuilder } from "discord.js";
 import Type from "../../Moonlight";
 import { CommandBuilder } from "../../structures/CommandBuilder";
+import { MoonlightEmbedBuilder } from "../../structures/MoonlightEmbedBuilder";
 
 export default new CommandBuilder({
     name: "mdn",
     description: "Muestra información de la documentación de Mozilla Developer Network.",
     cooldown: 3,
     usage: "<busqueda>",
-    example: "Array.propotype.map()",
+    example: "Array.prototype.map()",
     enabled: true,
     ownerOnly: true,
     async run(bot, msg, args) {
-	try {
-	    if (!args[1]) return msg.channel.send(bot.replyMessage("escribe lo que quieres buscar en la documentación.", { mention: msg.author.username, emoji: "noargs" }));
+	    try {
+	        if (!args[1]) return msg.channel.send(bot.replyMessage("escribe lo que quieres buscar en la documentación.", { mention: msg.author.username, emoji: "noargs" }));
 
-	    const data = await fetch(`https://developer.mozilla.org/api/v1/search?q=${encodeURIComponent(args.slice(1).join(" "))}&locale=es`).then(res => res.json());
-	    let message: Array<string> = [];
+	        const data = await fetch(`https://developer.mozilla.org/api/v1/search?q=${encodeURIComponent(args.slice(1).join(" "))}&locale=es`).then(res => res.json());
+	        let message: Array<string> = [];
 
-	    console.log(data.documents);
-	    for (let x = 0; x < data.documents.length; x++) {
-			message.push(`${x+1}. ${data.documents[x].title}`);
-	    }
-	    console.log(message);
+            for (const doc of data.documents) {
+                message.push(`> [${doc.title}](https://developer.mozilla.org${doc.mdn_url})`);
+            }
 
-	    const embed = new EmbedBuilder()
-	    .setTitle(`Resultados con ${args[1]}`)
-	    .setDescription(message.join("\n"))
-	    .setColor("Random")
-
-	    return msg.reply({ embeds: [embed] });
-	} catch (err) {
+            const embed = new MoonlightEmbedBuilder(msg.author, msg.guild!)
+            .setDescription(message.join("\n"))
+            .setTitle(`Resultados con: ${args[1]}`);
+	        return msg.reply({ embeds: [embed] });
+	    } catch (err) {
             bot.error("Hubo un error al intentar obtener los datos de la documentación.", { name: this.name, type: Type.Command, channel: msg.channel, error: err });
-	}
+	    }
     }
 });
