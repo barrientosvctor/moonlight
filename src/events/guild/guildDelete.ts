85783ce62f386548389d5d469a4e33ff66a95e45
barrientosvctor <vibarrientosp@gmail.com>
2023-01-29 13:50:59 -0500

    fix: info not available

--- src/events/guild/guildDelete.ts
+++ src/events/guild/guildDelete.ts
@@ -8,9 +8,9 @@ export default new EventBuilder({
         try {
             const embed = new EmbedBuilder()
             .setThumbnail(guild.iconURL({ extension: "png", size: 2048 }))
-            .setTitle(`${bot.user.username} fue sacado de un servidor!`)
+            .setTitle(`${bot.user?.username} fue sacado de un servidor!`)
             .setColor("Red")
-            .addFields({ name: `• Información del servidor`, value: `\`\`\`diff\n+ Nombre: ${guild.name}\n+ Propietario: ${bot.users.cache.get(guild.ownerId).username}#${bot.users.cache.get(guild.ownerId).discriminator}\n+ Server ID: ${guild.id}\n+ Miembros: ${guild.memberCount} (Humanos: ${guild.members.cache.filter((m) => !m.user.bot).size})\n\`\`\`` })
+            .addFields({ name: `• Información del servidor`, value: `\`\`\`diff\n+ Nombre: ${guild.name}\n+ Propietario: ${bot.users.cache.get(guild.ownerId)?.username}#${bot.users.cache.get(guild.ownerId)?.discriminator}\n+ Server ID: ${guild.id}\n+ Miembros: ${guild.memberCount} (Humanos: ${guild.members.cache.filter((m) => !m.user.bot).size})\n\`\`\`` })
             .setTimestamp();
             bot.hook.send({ embeds: [embed] });
         } catch (err) {
