549eb2bcbcf384011024df0365ca9eb92dd7e9d6
barrientosvctor <vibarrientosp@gmail.com>
2023-05-10 15:17:02 -0500

    feat(types): clash royale

---
+++ src/types/clashRoyale.ts
@@ -0,0 +1,9 @@
+export interface ClashRoyaleDeckData {
+  name: string;
+  id: number;
+  level: number;
+  startLevel: number;
+  maxLevel: number;
+  count: number;
+  iconUrls: { medium: string; };
+}
