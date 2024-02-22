0a6fc082d5fa52bba8f758bbe784b1f6904be7ae
barrientosvctor <vibarrientosp@gmail.com>
2024-02-21 21:53:36 -0500

    feat(EventBuilder): better types

--- src/structures/EventBuilder.ts
+++ src/structures/EventBuilder.ts
@@ -1,13 +1,22 @@
-import { EventBuilderParameters } from "../types/eventBuilder.js";
+import type { Awaitable, ClientEvents } from "discord.js";
+import type { MoonlightClient } from "./Client.js";
 
-export class EventBuilder implements EventBuilderParameters {
-  name: EventBuilderParameters["name"];
-  once?: EventBuilderParameters["once"];
-  execute: EventBuilderParameters["execute"];
+export class EventBuilder<Event extends keyof ClientEvents = keyof ClientEvents> implements EventBuilderParameters<Event> {
+  event: EventBuilderParameters<Event>["event"];
+  once?: EventBuilderParameters<Event>["once"];
+  execute: EventBuilderParameters<Event>["execute"];
 
-  constructor(private readonly __params: EventBuilderParameters) {
-    this.name = __params.name;
+  constructor(protected readonly __params: EventBuilderParameters<Event>) {
+    this.event = __params.event;
     this.once = __params.once ?? false;
     this.execute = __params.execute;
   }
 }
+
+type EventBuilderParameters<T> = {
+  event: T extends keyof ClientEvents ? T : keyof ClientEvents;
+  once?: boolean;
+  execute: (...args: EventArgs<T>) => Awaitable<void>;
+}
+
+type EventArgs<T> = T extends keyof ClientEvents ? [...args: ClientEvents[T], client: MoonlightClient] : unknown[];
--- src/types/eventBuilder.ts
+++
@@ -1,8 +0,0 @@
-import { ClientEvents } from "discord.js";
-import { MoonlightClient } from "../structures/Client.js";
-
-export type EventBuilderParameters = {
-  name: keyof ClientEvents;
-  once?: boolean;
-  execute: (client: MoonlightClient, ...args: any[]) => void;
-}
