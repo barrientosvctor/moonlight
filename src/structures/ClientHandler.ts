9987ca638c1e03ef53f44144e72f3880cca1066c
barrientosvctor <vibarrientosp@gmail.com>
2024-02-21 21:57:27 -0500

    feat(handler): event executing process using new way

--- src/structures/ClientHandler.ts
+++ src/structures/ClientHandler.ts
@@ -20,13 +20,13 @@ export class ClientHandler implements ClientHandlerPieces {
       throw new Error("Event handler could not be resolved.");
 
     result.value.filter(file => file.name.endsWith(this.__path.extension)).forEach(async info => {
-      const event: EventBuilder = (await import(`../events/${info.name}`)).default;
+      const event = (await import(`../events/${info.name}`)).default as EventBuilder;
       console.log(event)
 
       if (event.once)
-        this.__client.once(event.name, event.execute.bind(null, this.__client));
+        this.__client.once(event.event, (...args) => event.execute(...args, this.__client));
       else
-        this.__client.on(event.name, event.execute.bind(null, this.__client));
+        this.__client.on(event.event, (...args) => event.execute(...args, this.__client));
     });
 
     console.log(result.value)
