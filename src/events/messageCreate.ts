5e77b53668a110a76e0d29a700e468ac12314e3b
barrientosvctor <vibarrientosp@gmail.com>
2024-02-24 21:05:19 -0500

    feat(events): messageCreate

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,26 @@
+import { EventBuilder } from "../structures/EventBuilder.js";
+import { CommandType } from "../types/command.types.js";
+
+export default new EventBuilder({
+  event: "messageCreate",
+  async execute(message, client) {
+    const prefix = "!!" as const;
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.substring(prefix.length).split(/\s+/g);
+
+    if (!args[0]) return;
+
+    const command = client.commandsManager.getCommand(args[0], CommandType.Legacy);
+
+    if (!command) return;
+
+    try {
+      await command.run(client, message, args);
+    } catch (error) {
+      console.log("[ERROR]: Event could not running a command.");
+      console.error(error);
+    }
+  }
+});
