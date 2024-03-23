f2414f0813a739167bd37e142a30a5d284deb07c
barrientosvctor <vibarrientosp@gmail.com>
2024-03-23 00:09:34 -0500

    feat(types): client

---
+++ src/types/client.types.ts
@@ -0,0 +1,39 @@
+import { CommandManager } from "../structures/CommandManager.js";
+import jsonUtils from "../util/data.json" with { type: "json" };
+
+export type Emoji = {
+  check: string | string[];
+  error: string | string[];
+  noargs: string | string[];
+  sad: string | string[];
+  tada: string | string[];
+  wait: string | string[];
+  warning: string | string[];
+  love: string | string[];
+}
+
+export const emojiList: Emoji = {
+  check: ["✅"],
+  error: ["❌"],
+  noargs: ["❗"],
+  sad: ["😔", "😕", "😞", "😟", "🙁", "☹️", "😢", "😭"],
+  tada: ["🎉"],
+  wait: ["<a:waiting:1019010655434571969>"],
+  warning: ["⚠️"],
+  love: ["❤️"]
+} as const;
+
+export type EmojiType = keyof Emoji;
+
+export type BeautyMessageOptions = {
+  mention: string;
+  emoji: EmojiType;
+}
+
+export type ClientPieces = {
+  commandsManager: CommandManager;
+  cooldown: Map<string, Map<string, number>>;
+  utils: typeof jsonUtils;
+  getEmoji(type: EmojiType): string[] | string;
+  beautifyMessage(message: string, data: Partial<BeautyMessageOptions>): string;
+}
