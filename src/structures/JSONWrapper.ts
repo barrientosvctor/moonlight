cb07320f431c73aa25640f65709eb5a9250e7bcb
barrientosvctor <vibarrientosp@gmail.com>
2024-03-25 21:23:16 -0500

    feat(structures): JSONWrapper

---
+++ src/structures/JSONWrapper.ts
@@ -0,0 +1,16 @@
+import jsonData from "../util/data.json" with { type: "json" };
+
+type JSONKey = keyof typeof jsonData;
+
+type JSONWrapperPieces = {
+  get<ItemType extends string>(key: JSONKey, item: ItemType): string;
+}
+
+export class JSONWrapper implements JSONWrapperPieces {
+  private readonly data = jsonData;
+
+  get<ItemType extends string>(key: JSONKey, item: ItemType) {
+    const index = Object.keys(this.data[key]).indexOf(item);
+    return Object.values(this.data[key])[index];
+  }
+}
