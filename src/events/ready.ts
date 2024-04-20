98efd2672db2cc308d8d2b9a8847989b71c4d127
barrientosvctor <vibarrientosp@gmail.com>
2024-04-20 14:18:14 -0500

    chore: delete application fetch on ready event
    before this commit, that was a dead code line

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,7 +4,6 @@ export default new EventBuilder({
   event: "ready",
   once: true,
   async execute(client) {
-    await client.application.fetch();
     console.log(`${client.user.tag} is online!`);
   }
 });
