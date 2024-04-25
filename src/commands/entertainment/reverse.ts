1c6526312bdd9d9d7ba45ccfc310e9dbac3b7d32
barrientosvctor <vibarrientosp@gmail.com>
2024-04-25 18:52:28 -0500

    feat(commands): reverse

---
+++ src/commands/entertainment/reverse.ts
@@ -0,0 +1,14 @@
+import { CommandBuilder } from "../../structures/CommandBuilder.js";
+import { CommandType } from "../../types/command.types.js";
+
+export default new CommandBuilder({
+  type: CommandType.Legacy,
+  name: "reverse",
+  description: "Pon en reversa un texto.",
+  cooldown: 5,
+  category: "Entretenimiento",
+  async run(client, message, args) {
+if (!args[1]) return message.channel.send(client.beautifyMessage("escibe el texto que pondré en reversa.", { mention: message.author.username, emoji: "noargs" }));
+      return message.reply(args.slice(1).join(" ").split("").reverse().join(""));
+  }
+});
