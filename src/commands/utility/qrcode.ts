17fd8627074e6af085a9746b0655b5165de3d3f2
barrientosvctor <vibarrientosp@gmail.com>
2024-04-25 18:08:34 -0500

    style: code style according to prettier config

--- src/commands/utility/qrcode.ts
+++ src/commands/utility/qrcode.ts
@@ -1,7 +1,7 @@
 import { EmbedBuilder, bold } from "discord.js";
 import { CommandBuilder } from "../../structures/CommandBuilder.js";
 import { CommandType } from "../../types/command.types.js";
-const HTTP_Regex = /^(http|https):///g
+const HTTP_Regex = /^(http|https):/; //g
 
 export default new CommandBuilder({
   type: CommandType.Legacy,
@@ -13,13 +13,27 @@ export default new CommandBuilder({
   example: "https://youtube.com",
   category: "Utilidad",
   async run(client, message, args) {
-if (!args[1]) return message.channel.send(client.beautifyMessage("escribe la URL que contendrá el código QR.", { mention: message.author.username, emoji: "noargs" }));
-      if (!HTTP_Regex.test(args[1])) return message.reply(client.beautifyMessage("Tu URL es inválida, asegurate de que empiece con el protocolo HTTP.", { emoji: "error" }));
+    if (!args[1])
+      return message.channel.send(
+        client.beautifyMessage("escribe la URL que contendrá el código QR.", {
+          mention: message.author.username,
+          emoji: "noargs"
+        })
+      );
+    if (!HTTP_Regex.test(args[1]))
+      return message.reply(
+        client.beautifyMessage(
+          "Tu URL es inválida, asegurate de que empiece con el protocolo HTTP.",
+          { emoji: "error" }
+        )
+      );
 
-      const embed = new EmbedBuilder()
-    .setColor("Random")
+    const embed = new EmbedBuilder()
+      .setColor("Random")
       .setDescription(`> ${bold("URL")}: ${args[1]}`)
-      .setImage(`https://api.qrserver.com/v1/create-qr-code/?size=1024x1024&data=${args[1]}`)
-      return message.reply({ embeds: [embed] });
+      .setImage(
+        `https://api.qrserver.com/v1/create-qr-code/?size=1024x1024&data=${args[1]}`
+      );
+    return message.reply({ embeds: [embed] });
   }
 });
--- src/commands/utility/weather.ts
+++ src/commands/utility/weather.ts
@@ -11,14 +11,28 @@ export default new CommandBuilder({
   example: "Texas",
   category: "Utilidad",
   async run(client, message, args) {
-if (!args[1]) return message.channel.send(client.beautifyMessage("escribe el nombre de un país o ciudad para obtener el pronóstico del clima.", { mention: message.author.username, emoji: "noargs" }))
-      const data = await fetch(`https://api.popcat.xyz/weather?q=${encodeURIComponent(args.slice(1).join(" "))}`).then(res => res.json());
-      if (!data) return message.reply(client.beautifyMessage("No encontré esta ubicación.", { emoji: "error" }));
+    if (!args[1])
+      return message.channel.send(
+        client.beautifyMessage(
+          "escribe el nombre de un país o ciudad para obtener el pronóstico del clima.",
+          { mention: message.author.username, emoji: "noargs" }
+        )
+      );
+    const data = await fetch(
+      `https://api.popcat.xyz/weather?q=${encodeURIComponent(args.slice(1).join(" "))}`
+    ).then(res => res.json());
+    if (!data)
+      return message.reply(
+        client.beautifyMessage("No encontré esta ubicación.", {
+          emoji: "error"
+        })
+      );
 
-      const embed = new EmbedBuilder()
-    .setColor("Random")
+    const embed = new EmbedBuilder()
+      .setColor("Random")
       .setTitle(`${data[0].location.name}`)
-      .setDescription(`
+      .setDescription(
+        `
 > ${underscore("Pronóstico del clima")}
 ${bold("Cielo")}: ${data[0].current.skytext}
 ${bold("Temperatura")}: ${data[0].current.temperature}°${data[0].location.degreetype}
@@ -29,8 +43,9 @@ ${bold("Velocidad del viento")}: ${data[0].current.winddisplay}
 > ${underscore("Otra información")}
 ${bold("Día")}: ${data[0].current.day} (${data[0].current.shortday})
 ${bold("Fecha y hora")}: ${data[0].current.date} - ${data[0].current.observationtime}
-${bold("Zona horaria")}: GMT${data[0].location.timezone}`)
+${bold("Zona horaria")}: GMT${data[0].location.timezone}`
+      )
       .setThumbnail(data[0].current.imageUrl);
-      return message.reply({ embeds: [embed] });
+    return message.reply({ embeds: [embed] });
   }
 });
