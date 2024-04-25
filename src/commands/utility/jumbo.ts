a7b6fd6d99f0e9134889d96597f61737be13570e
barrientosvctor <vibarrientosp@gmail.com>
2024-04-24 23:36:54 -0500

    feat(commands): jumbo

---
+++ src/commands/utility/jumbo.ts
@@ -0,0 +1,41 @@
+import { bold, time } from "discord.js";
+import { CommandBuilder } from "../../structures/CommandBuilder.js";
+import { CommandType } from "../../types/command.types.js";
+
+export default new CommandBuilder({
+  type: CommandType.Legacy,
+  name: "jumbo",
+  description:
+    "Obten la imagen de un emoji del servidor, añadido también con información sobre este.",
+  cooldown: 10,
+  usage: "<:emoji:>",
+  example: ":RappiHappu:",
+  category: "Utilidad",
+  async run(client, message, args) {
+    if (!message.inGuild()) return;
+    if (!args[1])
+      return message.channel.send(
+        client.beautifyMessage("Debes poner un emoji", {
+          mention: message.author.username,
+          emoji: "noargs"
+        })
+      );
+
+    const emoji = message.guild.emojis.cache.find(
+      emoji => emoji.name === args[1].split(":")[1]
+    );
+    if (!emoji)
+      return message.reply(
+        client.beautifyMessage("Ese emoji no se encontró en este servidor.", {
+          emoji: "error"
+        })
+      );
+
+    return message.reply(`
+> ${bold("Nombre")}: \`${emoji.name}\`
+> ${bold("ID")}: \`${emoji.id}\`
+> ${bold("Identificador")}: \`${emoji.animated ? `<a:${emoji.name}:${emoji.id}>` : `<:${emoji.name}:${emoji.id}>`}\`
+> ${bold("Fecha de creación")}: ${time(Math.ceil(emoji.createdTimestamp / 1000))}
+> ${bold("URL")}: ${emoji.imageURL({ size: 2048, extension: emoji.animated ? "gif" : "png" })}`);
+  }
+});
