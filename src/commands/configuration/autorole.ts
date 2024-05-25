a822aa67e3ddbb05e2d8ded1d6c6cd8a483f63c8
barrientosvctor <vibarrientosp@gmail.com>
2024-05-25 00:17:33 -0500

    style: code style according to prettier config

--- src/commands/configuration/autorole.ts
+++ src/commands/configuration/autorole.ts
@@ -8,7 +8,8 @@ export default new CommandBuilder({
   name: "autorole",
   cooldown: 10,
   category: "Configuración",
-  description: "Establece un rol a los nuevos usuarios o bots que entren al servidor.",
+  description:
+    "Establece un rol a los nuevos usuarios o bots que entren al servidor.",
   usage: "<set / delete / list> <user / bot> <@rol | ID>",
   example: "set user @Usuarios",
   requiredMemberPermissions: ["ManageRoles"],
@@ -16,48 +17,121 @@ export default new CommandBuilder({
   async run(client, message, args) {
     if (!message.inGuild() || !message.member) return;
     if (!args[1])
-    return message.reply(client.beautifyMessage("Escribe una de las siguientes opciones: `set`, `delete` ó `list`.", { emoji: "noargs" }));
+      return message.reply(
+        client.beautifyMessage(
+          "Escribe una de las siguientes opciones: `set`, `delete` ó `list`.",
+          { emoji: "noargs" }
+        )
+      );
 
     if (args[1] === "set") {
-      if (!["user", "bot"].includes(args[2])) return message.reply(client.beautifyMessage("Elige `user` ó `bot`.", { emoji: "noargs" }));
+      if (!["user", "bot"].includes(args[2]))
+        return message.reply(
+          client.beautifyMessage("Elige `user` ó `bot`.", { emoji: "noargs" })
+        );
       const key = `${args[2]}-${message.guildId}`;
 
       const role = getRole(args[3], message);
-      if (!role) return message.reply(client.beautifyMessage("Este rol no existe, prueba con otro.", { emoji: "error" }));
-      if (role.position >= message.guild.members.me?.roles.highest.position!) return message.reply(client.beautifyMessage("No puedo añadir este rol debido a que jerárquicamente tiene un puesto mayor o igual al mío.", { emoji: "error" }));
-      if (role.position >= message.member.roles.highest.position) return message.reply(client.beautifyMessage("No puedes añadir el rol ya que jerárquicamente tiene un puesto mayor o igual al tuyo!", { emoji: "error" }));
-      if (role.managed) return message.reply(client.beautifyMessage("No puedo asignar roles que estén administrados por una integración, prueba con otro.", { emoji: "warning" }));
+      if (!role)
+        return message.reply(
+          client.beautifyMessage("Este rol no existe, prueba con otro.", {
+            emoji: "error"
+          })
+        );
+      if (role.position >= message.guild.members.me?.roles.highest.position!)
+        return message.reply(
+          client.beautifyMessage(
+            "No puedo añadir este rol debido a que jerárquicamente tiene un puesto mayor o igual al mío.",
+            { emoji: "error" }
+          )
+        );
+      if (role.position >= message.member.roles.highest.position)
+        return message.reply(
+          client.beautifyMessage(
+            "No puedes añadir el rol ya que jerárquicamente tiene un puesto mayor o igual al tuyo!",
+            { emoji: "error" }
+          )
+        );
+      if (role.managed)
+        return message.reply(
+          client.beautifyMessage(
+            "No puedo asignar roles que estén administrados por una integración, prueba con otro.",
+            { emoji: "warning" }
+          )
+        );
 
-      if (client.database.has("autorole", key) && client.database.get("autorole", key) === role.id) return message.reply(client.beautifyMessage(`Este rol ya ha sido establecido anteriormente para los ${args[2] === "user" ? "usuarios" : "bots"}, prueba con otro.`, { emoji: "warning" }));
+      if (
+        client.database.has("autorole", key) &&
+        client.database.get("autorole", key) === role.id
+      )
+        return message.reply(
+          client.beautifyMessage(
+            `Este rol ya ha sido establecido anteriormente para los ${args[2] === "user" ? "usuarios" : "bots"}, prueba con otro.`,
+            { emoji: "warning" }
+          )
+        );
 
       if (client.database.has("autorole", key))
         await client.database.modify("autorole", key, role.id);
-      else
-        await client.database.add("autorole", key, role.id);
+      else await client.database.add("autorole", key, role.id);
 
-      return message.reply(client.beautifyMessage(`A partir de ahora, éste rol será asignado a todos los ${args[2] === "user" ? "usuarios" : "bots"} que entren al servidor.`, { emoji: "check" }));
+      return message.reply(
+        client.beautifyMessage(
+          `A partir de ahora, éste rol será asignado a todos los ${args[2] === "user" ? "usuarios" : "bots"} que entren al servidor.`,
+          { emoji: "check" }
+        )
+      );
     } else if (args[1] === "delete") {
-      if (!["user", "bot"].includes(args[2])) return message.reply(client.beautifyMessage("Elige `user` ó `bot`.", { emoji: "noargs" }));
+      if (!["user", "bot"].includes(args[2]))
+        return message.reply(
+          client.beautifyMessage("Elige `user` ó `bot`.", { emoji: "noargs" })
+        );
       const key = `${args[2]}-${message.guildId}`;
 
-      if (!client.database.has("autorole", key)) return message.reply(client.beautifyMessage(`No hay ningún rol para eliminar. Para añadir uno, usa el comando: ${inlineCode(`!!${this.name} set ${args[2]} @rol`)}`, { emoji: "error" }));
+      if (!client.database.has("autorole", key))
+        return message.reply(
+          client.beautifyMessage(
+            `No hay ningún rol para eliminar. Para añadir uno, usa el comando: ${inlineCode(`!!${this.name} set ${args[2]} @rol`)}`,
+            { emoji: "error" }
+          )
+        );
 
       await client.database.delete("autorole", key);
 
-      return message.reply(client.beautifyMessage("Rol eliminado de la lista éxitosamente.", { emoji: "check" }));
+      return message.reply(
+        client.beautifyMessage("Rol eliminado de la lista éxitosamente.", {
+          emoji: "check"
+        })
+      );
     } else if (args[1] == "list") {
       const list = [];
       if (client.database.has("autorole", `user-${message.guildId}`))
-        list.push(`- ${bold("Usuarios")}: ${roleMention(client.database.get("autorole", `user-${message.guildId}`)!)}`);
+        list.push(
+          `- ${bold("Usuarios")}: ${roleMention(client.database.get("autorole", `user-${message.guildId}`)!)}`
+        );
 
       if (client.database.has("autorole", `bot-${message.guildId}`))
-        list.push(`- ${bold("Bots")}: ${roleMention(client.database.get("autorole", `bot-${message.guildId}`)!)}`);
+        list.push(
+          `- ${bold("Bots")}: ${roleMention(client.database.get("autorole", `bot-${message.guildId}`)!)}`
+        );
 
-      if (list.length === 0) return message.reply(client.beautifyMessage("No hay roles establecidos para mostrar acá.", { emoji: "error" }));
+      if (list.length === 0)
+        return message.reply(
+          client.beautifyMessage(
+            "No hay roles establecidos para mostrar acá.",
+            { emoji: "error" }
+          )
+        );
 
       return message.reply(`# Lista de autoroles
 
 ${list.join("\n")}`);
-    } else return message.reply(client.beautifyMessage("Opción no válida. Elige alguna de las opciones: `set`, `delete` ó `list`.", { emoji: "error" }));
+    } else
+      return message.reply(
+        client.beautifyMessage(
+          "Opción no válida. Elige alguna de las opciones: `set`, `delete` ó `list`.",
+          { emoji: "error" }
+        )
+      );
   }
 });
--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -3,17 +3,37 @@ import { EventBuilder } from "../structures/EventBuilder.js";
 export default new EventBuilder({
   event: "guildMemberAdd",
   async execute(member, client) {
-    if (client.database.has("autorole", `user-${member.guild.id}`) && !member.roles.cache.has(client.database.get("autorole", `user-${member.guild.id}`)!) && !member.user.bot) {
+    if (
+      client.database.has("autorole", `user-${member.guild.id}`) &&
+      !member.roles.cache.has(
+        client.database.get("autorole", `user-${member.guild.id}`)!
+      ) &&
+      !member.user.bot
+    ) {
       try {
-        await member.roles.add(client.database.get("autorole", `user-${member.guild.id}`)!)
+        await member.roles.add(
+          client.database.get("autorole", `user-${member.guild.id}`)!
+        );
       } catch (error) {
-        console.error(`I couldn't add the user autorole in ${member.guild.name}.`);
+        console.error(
+          `I couldn't add the user autorole in ${member.guild.name}.`
+        );
       }
-    } else if (client.database.has("autorole", `bot-${member.guild.id}`) && !member.roles.cache.has(client.database.get("autorole", `bot-${member.guild.id}`)!) && member.user.bot) {
+    } else if (
+      client.database.has("autorole", `bot-${member.guild.id}`) &&
+      !member.roles.cache.has(
+        client.database.get("autorole", `bot-${member.guild.id}`)!
+      ) &&
+      member.user.bot
+    ) {
       try {
-        await member.roles.add(client.database.get("autorole", `bot-${member.guild.id}`)!);
+        await member.roles.add(
+          client.database.get("autorole", `bot-${member.guild.id}`)!
+        );
       } catch (error) {
-        console.error(`I couldn't add the bot autorole in ${member.guild.name}.`);
+        console.error(
+          `I couldn't add the bot autorole in ${member.guild.name}.`
+        );
       }
     }
   }
