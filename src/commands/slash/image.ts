d8e4cc8d20609510bb2044111b7f7db2a08e78b8
barrientosvctor <vibarrientosp@gmail.com>
2024-11-10 23:09:15 -0500

    Add image cat command

--- src/commands/slash/image.ts
+++ src/commands/slash/image.ts
@@ -17,7 +17,17 @@ export default new SlashCommand({
   async run(interaction, _) {
     const subcommand = interaction.options.getSubcommand();
     if (subcommand === "cat") {
-      return interaction.reply("meow!");
+      const data = await fetch(`https://api.thecatapi.com/v1/images/search`).then(res => res.json());
+      if (!data)
+        return interaction.reply("No fue posible encontrar una imagen.");
+
+      const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setDescription("¡Mira este lindo gatito!")
+      .setImage(data[0].url)
+      .setFooter({ text: `Tamaño: ${data[0].width}x${data[0].height}` });
+
+      return interaction.reply({ embeds: [embed] });
     } else if (subcommand === "dog") {
       const data = await fetch("https://api.thedogapi.com/v1/images/search", {
         method: "GET"
