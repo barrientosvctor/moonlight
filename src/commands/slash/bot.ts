99624a58101b7eccf1c0d9e152cd9398a57f90ae
barrientosvctor <vibarrientosp@gmail.com>
2024-11-11 18:06:44 -0500

    Add bot ping command

--- src/commands/slash/bot.ts
+++ src/commands/slash/bot.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, hyperlink } from "discord.js";
+import { EmbedBuilder, SlashCommandBuilder, hyperlink } from "discord.js";
 import { SlashCommand } from "../../structures/CommandBuilder.js";
 
 export default new SlashCommand({
@@ -8,12 +8,24 @@ export default new SlashCommand({
   .addSubcommand(cmd =>
     cmd
     .setName("invite")
-    .setDescription("Invita a Moonlight a tu servidor.")),
+    .setDescription("Invita a Moonlight a tu servidor."))
+  .addSubcommand(cmd =>
+    cmd
+    .setName("ping")
+    .setDescription("Revisa el tiempo de respuesta de Moonlight.")),
   testGuildOnly: true,
   async run(interaction, client) {
     const subcommand = interaction.options.getSubcommand();
     if (subcommand === "invite") {
       return interaction.reply(`Gracias por considerar agregarme a tu servidor. Presiona ${hyperlink("aquí", client.wrapper.get("bot.info", "invite"))} para invitarme. :heart:`);
+    } else if (subcommand === "ping") {
+      const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setDescription(`
+Mensajes: \`${Date.now() - interaction.createdTimestamp}ms\`
+WebSocket: \`${client.ws.ping}ms\`
+`)
+      return interaction.reply({ content: "Pong! :ping_pong:", embeds: [embed] });
     }
 
     return interaction.reply("Haz uso de la variedad de comandos que ofrece éste comando.");
