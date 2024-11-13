917e1c4e28ff0b031e2084f2c6fcaa57be293d83
barrientosvctor <vibarrientosp@gmail.com>
2024-11-13 17:40:45 -0500

    Add reaction slap command

--- src/commands/slash/reaction.ts
+++ src/commands/slash/reaction.ts
@@ -188,6 +188,21 @@ export default new SlashCommand({
         .setColor("Random")
         .setImage(data.url);
       return interaction.reply({ embeds: [embed] });
+    } else if (subcommand === "slap") {
+      const member = interaction.options.getMember("member") as GuildMember;
+      data = await fetchAnimeGIF("slap");
+      const embed = new EmbedBuilder();
+
+      if (!member)
+        return interaction.reply({ content: "No pude encontrar a esa persona en el servidor.", ephemeral: true });
+      if (member.user.id === interaction.user.id)
+        return interaction.reply({ content: "¿Enserio quieres hacerte daño?", ephemeral: true });
+
+      embed
+        .setColor("Random")
+        .setImage(data.url)
+        .setDescription(`¡${bold(interaction.user.username)} le dio una bofetada a ${bold(member.user.username)}!`);
+      return interaction.reply({ embeds: [embed] });
     }
 
     return interaction.reply({ content: "Haz uso de los diferentes subcomandos que tiene este comando.", ephemeral: true });
